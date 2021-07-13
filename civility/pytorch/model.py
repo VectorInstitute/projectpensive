34a9690f86bb8339db9189b87ebf04a3fbc02e50
MichaelNasello <mlnasell@uwaterloo.ca>
2021-07-12 20:35:42 -0400

    Changing per_device_train_batch_size to 256

--- civility/pytorch/model.py
+++ civility/pytorch/model.py
@@ -37,7 +37,7 @@ class CivilCommentsModel:
         self.training_args = TrainingArguments(
             output_dir='./results',
             num_train_epochs=num_train_epochs,
-            per_device_train_batch_size=16,
+            per_device_train_batch_size=256,
             per_device_eval_batch_size=64,
             warmup_steps=500,
             weight_decay=0.01,
