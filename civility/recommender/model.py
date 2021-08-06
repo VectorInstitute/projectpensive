f560698573f6e9a8c56bba882f61cf9b5a3a9a23
MichaelNasello <mlnasell@uwaterloo.ca>
2021-08-06 16:16:57 -0400

    Updates to model and training loop

--- civility/recommender/model.py
+++ civility/recommender/model.py
@@ -3,22 +3,32 @@ from transformers import BertTokenizer, BertModel
 
 
 class RecommenderEngine(torch.nn.Module):
-    def __init__(self, data, n_users, n_factors, device):
+    def __init__(self, data, n_factors, device):
         super().__init__()
 
         self.data = data
         self.n_factors = n_factors
         self.device = device
 
-        # User embeddings
+        # Author embeddings
         self.author_lookup = {
             author: i for i, author in enumerate(self.data.author.unique())
         }
         self.author_embedding = torch.nn.Embedding(
-            n_users,
-            n_factors,
-            sparse=False
+            len(self.author_lookup),
+            n_factors // 2,
         )
+        self.author_linear = torch.nn.Linear(n_factors // 2, 100 // 2)
+
+        # Subreddit embeddings
+        self.subreddit_lookup = {
+            subreddit: i for i, subreddit in enumerate(self.data.subreddit.unique())
+        }
+        self.subreddit_embedding = torch.nn.Embedding(
+            len(self.subreddit_lookup),
+            n_factors // 2
+        )
+        self.subreddit_linear = torch.nn.Linear(n_factors // 2, 100 // 2)
 
         # Comment embeddings
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
@@ -30,31 +40,59 @@ class RecommenderEngine(torch.nn.Module):
         #self.text_embedding = torch.nn.Linear(self.bert_output_dim, len(self.author_lookup) * self.n_factors)
         self.text_lookup = {comment: i for i, comment in enumerate(self.data.comment.unique())}
         self.text_embedding = torch.nn.Embedding(
-            4,
+            len(self.text_lookup),
             n_factors,
         )
+        self.text_linear = torch.nn.Linear(n_factors, 100)
 
-    def forward(self, author, comment):
+        self.last_linear = torch.nn.Linear(100, 20)
+        self.last_linear2 = torch.nn.Linear(20, 1)
 
-        # Query pre-processing
+    def forward(self, author, subreddit, comment):
+
+        # Author pre-processing
         author = [self.author_lookup[i] for i in author]
-        author = torch.nn.functional.one_hot(torch.tensor(author), num_classes=len(self.author_lookup))
-        author = author.to(self.device)
+        author = torch.Tensor(author).long().to(self.device)
+        #author = torch.nn.functional.one_hot(torch.tensor(author), num_classes=len(self.author_lookup))
+        #author = author.to(self.device)
+
+        # Subreddit pre-processing
+        subreddit = [self.subreddit_lookup[i] for i in subreddit]
+        #subreddit = torch.nn.functional.one_hot(torch.tensor(subreddit), num_classes=len(self.subreddit_lookup))
+        #subreddit = subreddit.to(self.device)
+        subreddit = torch.Tensor(subreddit).long().to(self.device)
 
         # Comment pre-processing
-        tokenized = [self.tokenizer(c, return_tensors="pt") for c in comment]
+        comment = [self.text_lookup[i] for i in comment]
+        #comment = torch.nn.functional.one_hot(torch.tensor(comment).long(), num_classes=len(self.text_lookup))
+        #comment_rep = self.text_embedding(comment.to(self.device))
+        comment = torch.Tensor(comment).long().to(self.device)
+
+        """"tokenized = [self.tokenizer(c, return_tensors="pt") for c in comment]
         bert_rep = [self.bert(**t.to(self.device)) for t in tokenized]
         bert_out = [b.last_hidden_state[0] for b in bert_rep]
         bert_mean = [torch.mean(bo, dim=0) for bo in bert_out]
         bert_stack = torch.stack(bert_mean)
-
-        # Matmul
-        author_rep = self.author_embedding(author)
-        """comment_rep = self.text_embedding(bert_stack)
+        comment_rep = self.text_embedding(bert_stack)
         comment_rep = comment_rep.view(-1, len(self.author_lookup), self.n_factors)
         comment_rep_transpose = torch.transpose(comment_rep, 1, 2)"""
-        comment = [self.text_lookup[i] for i in comment]
-        comment = torch.nn.functional.one_hot(torch.Tensor(comment).long(), num_classes=len(self.text_lookup))
+
+        # Query representation
+        author_rep = self.author_embedding(author)
+        author_rep = self.author_linear(author_rep)
+        subreddit_rep = self.subreddit_embedding(subreddit)
+        subreddit_rep = self.subreddit_linear(subreddit_rep)
+        query_rep = torch.cat((author_rep, subreddit_rep), dim=1)
+
+        # Comment representation
         comment_rep = self.text_embedding(comment)
-        comment_rep_transpose = torch.transpose(comment_rep, 1, 2)
-        return torch.matmul(author_rep, comment_rep_transpose).mean([1, 2])
+        comment_rep = self.text_linear(comment_rep)
+        comment_rep_transpose = torch.transpose(comment_rep, 0, 1)
+
+        # Score computation
+        # return torch.matmul(author_rep, comment_rep_transpose).mean([1])
+        #matmul = torch.matmul(author_rep, comment_rep_transpose)
+        mul = torch.multiply(query_rep, comment_rep)
+        last = self.last_linear(mul)
+        last = self.last_linear2(last)
+        return torch.squeeze(last)
--- civility/recommender/train.py
+++ civility/recommender/train.py
@@ -1,5 +1,6 @@
-import torch
 import pandas as pd
+from sklearn.metrics import precision_recall_fscore_support
+import torch
 
 from data import SarcasticDataset
 from model import RecommenderEngine
@@ -12,7 +13,7 @@ if __name__ == "__main__":
     data = data.drop(["label", "date", "created_utc", "parent_comment"], 1)
 
     # Note that batch_size is essentially doubled to include a negative pair
-    batch_size = 10
+    batch_size = 32
     data_set = SarcasticDataset(data)
     data_loader = torch.utils.data.DataLoader(data_set, shuffle=True, batch_size=batch_size, num_workers=0)
 
@@ -20,17 +21,18 @@ if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     # Define model
-    model = RecommenderEngine(data, len(data), n_factors=64, device=device)
+    model = RecommenderEngine(data, n_factors=150, device=device)
     loss_fn = torch.nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
-    # TODO: implement subreddit embeddings
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
 
     # Training loop
-    num_epochs = 5
+    num_epochs = 10
     model.to(device)
     for epoch in range(num_epochs):
         losses = []
+        f1s = []
+        precisions = []
+        recalls = []
         for b, batch in enumerate(data_loader):
 
             # Merge positive and negative cases in batch
@@ -43,17 +45,39 @@ if __name__ == "__main__":
             batch["match_score"] = batch["match_score"].type(torch.float32)
 
             # Compute loss
-            prediction = model(batch["author"], batch["comment"])
-            loss = loss_fn(prediction, batch["match_score"])
+            prediction = model(batch["author"], batch["subreddit"], batch["comment"])
+            loss = loss_fn(prediction, batch["match_score"].to(device))
+
+            # Compute and track metrics
+            metrics = precision_recall_fscore_support(
+                prediction.data > 0.5, batch["match_score"], warn_for=tuple()
+            )
+
             losses.append(loss)
+            f1s.append(metrics[2][0])
+            precisions.append(metrics[0][0])
+            recalls.append(metrics[1][0])
 
             # Back propagate
             loss.backward()
             optimizer.step()
 
             if b % 50 == 0:
-                print(f"Batch {b} || Loss: {torch.mean(torch.Tensor(losses))}")
-        print(f"Epoch {epoch} || Loss {torch.mean(torch.Tensor(losses))}")
+                loss_avg = torch.mean(torch.Tensor(losses))
+                f1_avg = torch.mean(torch.Tensor(f1s))
+                precision_avg = torch.mean(torch.Tensor(precisions))
+                recall_avg = torch.mean(torch.Tensor(recalls))
+
+                print(
+                    f"Batch {b: <9} | Loss: {loss_avg:.3f}, F1: {f1_avg:.3f}, Precision: {precision_avg:.3f}, "
+                    f"Recall: {recall_avg:.3f}"
+                )
+
+        print(
+            f"\nAfter Epoch {epoch + 1: <3} | Loss: {torch.mean(torch.Tensor(losses)):.3f}, "
+            f"F1: {torch.mean(torch.Tensor(f1s)):.3f}, Precision: {torch.mean(torch.Tensor(precisions)):.3f},"
+            f"Recall: {torch.mean(torch.Tensor(recalls)):.3f}"
+        )
 
     torch.save(model.state_dict(), "final_model")
     print("Training Program Complete")
