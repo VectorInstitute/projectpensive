a117a74fa043af5853da8d90218213fd9b72d02c
MichaelNasello <mlnasell@uwaterloo.ca>
2021-08-09 23:34:29 -0400

    Make dir if it doesn't exist

--- civility/classifier/download_model_checkpoints.sh
+++ civility/classifier/download_model_checkpoints.sh
@@ -1,4 +1,5 @@
 echo Downloading model checkpoints from remote server...
 
-scp mnas@v.vectorinstitute.ai:baiso/baiso/civility/classifier/output.log
+mkdir results
+scp mnas@v.vectorinstitute.ai:baiso/baiso/civility/classifier/output.log results/output.log
 scp -r mnas@v.vectorinstitute.ai:baiso/baiso/civility/classifier/results/final_model results/final_model
