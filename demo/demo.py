0b2a47f6d66d6accec1a920cc48a509dfc466085
Sheen Thusoo <sheenthusoo@gmail.com>
2021-08-10 23:21:07 -0400

    implement both algos and evaluations

--- demo/demo.py
+++ demo/demo.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from helpers import load_recommender_data, generate_feed, run_classifier, load_civility_data
+from diversity_methods import get_similar_comments, greedy_selection, topic_diversification, compute_diversity, compare_diversity
 
 
 def demo():
@@ -14,42 +15,83 @@ def demo():
         "It consists of a balanced selection of sarcastic/genuine comment replies, made by a variety of users in and "
         "posted in several subreddits. Lets take a look at the data..."
     )
-
+    
+    
     with st.spinner("Loading data..."):
         data = load_recommender_data()
         st.table(data.head(n=3))
 
     # Civility Filter
-    st.subheader("Civility Filter")
-    st.write(
-        "We leverage the Hugging Face transformer library to train transformer based NLP models on the civil_comments "
-        "dataset. A score is assigned to convey the level of civility present in a post."
-    )
-    st.write(
-        "To try out the civility classifier, write your own comments, or select from some examples from the dataset."
-    )
+#     st.subheader("Civility Filter")
+#     st.write(
+#         "We leverage the Hugging Face transformer library to train transformer based NLP models on the civil_comments "
+#         "dataset. A score is assigned to convey the level of civility present in a post."
+#     )
+#     st.write(
+#         "To try out the civility classifier, write your own comments, or select from some examples from the dataset."
+#     )
 
-    text_input = st.text_input(label="Provide a comment to compute its toxicity score...")
-    if text_input not in ["Provide a comment to compute its toxicity score...", ""]:
-        with st.spinner("Computing..."):
-            output = run_classifier(text_input)
-            if output > 0.5:
-                st.write(f"This comment is considered **uncivil**, with a toxicity score of {output:.3f}.")
-            else:
-                st.write(f"This comment is considered **civil**, with a toxicity score of {output:.3f}.")
+#     text_input = st.text_input(label="Provide a comment to compute its toxicity score...")
+#     if text_input not in ["Provide a comment to compute its toxicity score...", ""]:
+#         with st.spinner("Computing..."):
+#             output = run_classifier(text_input)
+#             if output > 0.5:
+#                 st.write(f"This comment is considered **uncivil**, with a toxicity score of {output:.3f}.")
+#             else:
+#                 st.write(f"This comment is considered **civil**, with a toxicity score of {output:.3f}.")
 
-    civil_dataset_options = load_civility_data()
-    select_text = st.selectbox("Select a phrase to compute its toxicity score...", civil_dataset_options)
-    if select_text:
-        with st.spinner("Computing..."):
-            output = run_classifier(text_input)
-            if output > 0.5:
-                st.write(f"This comment is considered **uncivil**, with a toxicity score of {output:.3f}.")
-            else:
-                st.write(f"This comment is considered **civil**, with a toxicity score of {output:.3f}.")
+#     civil_dataset_options = load_civility_data()
+#     select_text = st.selectbox("Select a phrase to compute its toxicity score...", civil_dataset_options)
+#     if select_text:
+#         with st.spinner("Computing..."):
+#             output = run_classifier(text_input)
+#             if output > 0.5:
+#                 st.write(f"This comment is considered **uncivil**, with a toxicity score of {output:.3f}.")
+#             else:
+#                 st.write(f"This comment is considered **civil**, with a toxicity score of {output:.3f}.")
 
     # Diversity Filter
     st.subheader("Diversity Filter")
+    diversity_algo_options = ("None", "Bounded Greedy Selection", "Topic Diversification")
+    st.markdown("Using the HuggingFace Sentence Transformers Library, we generated embeddings for each comment. We then implemented two diverity algorithms described below. Try out both and see how your recommendations change!")
+    with st.expander("1. Bounded Greedy Algorithm"):
+        st.markdown("""This algorithm seeks to provide a more principled approach to improving diversity by using a “quality” metric to construct the recommendation set, R, in an incremental fashion. The quality of an item is proportional to its similarity to the target query and its relative diversity to the items so far selected. The first item to be selected is the one with the highest similarity to the target query. During each subsequent iteration, the item selected is the one with the highest quality with respect to the set of items selected during the previous iteration.  
+    To reduce the complexity, we implemented a bounded version in which we first select the top k items according to their similarity to the target query and apply the Greedy Selection method to these.""")
+        col1, col2, col3 = st.columns([1,1,1])
+        col2.image("images/greedy_pseudo.png")
+    
+    with st.expander("2. Topic Diversification Algorithm"):
+        st.markdown("""
+        This algorithm seeks to tackle the problem of diversifying the topics of books recommended to users; here, we apply it to Reddit comments. We first generate recommended items for the target query (at least 5N for the top-N final recommendations). For each N+1 position item, we calculate the ILS (intralist similarity) if this item was part of the top-N list. Then we sort the remaining items in reverse (according to the ILS rank) to get their dissimilarity rank. We calculate the new rank for each item as r = a ∗ P + b ∗ Pd, with P being the original rank, Pd being the dissimilarity rank and a, b being constants in range [0, 1]. Lastly, we select the top-N items according to the newly calculated rank.
+        """)
+        col1, col2, col3 = st.columns([1,1,1])
+        col2.image("images/topic_pseudo.png")
+    
+    query = st.text_input(label="Provide a comment to get diverse recommendations")
+    algorithm = st.selectbox("Choose a diversity algorithm", diversity_algo_options)
+    if algorithm == diversity_algo_options[0]:
+        pass
+    elif algorithm == diversity_algo_options[1]:
+        if query not in ["Provide a comment to get diverse recommendations", ""]:
+            with st.spinner("Computing..."):
+                st.write("Recommendations computed with Bounded Greedy Selection:")
+                recommendations = greedy_selection(query, 10)[0]
+                st.write(recommendations)
+                avg_dissim_control = compute_diversity(get_similar_comments(query, 10)[1], 10)
+                avg_dissim_algo = compute_diversity(greedy_selection(query, 10)[1], 10)
+                st.write("Compared to a normal recommender, this algorithm increased diversity by " + 
+                         str(compare_diversity(avg_dissim_algo, avg_dissim_control)) + "%")
+    else:
+        if query not in ["Provide a comment to get diverse recommendations", ""]:
+            with st.spinner("Computing..."):
+                st.write("Recommendations computed with Topic Diversification:")
+                recommendations = topic_diversification(query, 10)[0]
+                st.write(recommendations)
+                avg_dissim_control = compute_diversity(get_similar_comments(query, 10)[1], 10)
+                avg_dissim_algo = compute_diversity(greedy_selection(query, 10)[1], 10)
+                st.write("Compared to a normal recommender, this algorithm increased diversity by " + 
+                         str(compare_diversity(avg_dissim_algo, avg_dissim_control)) + "%")
+
 
     # Applying filters to feed
     st.subheader("Putting It All Together")
@@ -106,6 +148,7 @@ def demo():
                 civility_threshold
             )
         elif diversity_filter:
+            st.radio("Select a Diversity Algorithm", diversity_algo_options)
             raise NotImplementedError("Done by sheen")
         else:
             feed = generate_feed(
--- demo/diversity_methods.py
+++ demo/diversity_methods.py
@@ -1,9 +1,31 @@
 import pandas as pd
 import numpy as np
 import torch
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.spatial.distance import pdist
+from sentence_transformers import SentenceTransformer, util
+from collections import OrderedDict
 
-sarcasm_embeddings = torch.load("data/sarcasm_embeddings.pt")
-df = pd.read_csv("data/sarcasm_dataframe.csv")
+embedder = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+
+sarcasm_embeddings = torch.load("data/sarcasm_embeddings.pt", map_location=torch.device('cpu'))
+
+def load_dataframe():
+    dataset = pd.read_csv("../civility/recommender/train-balanced-sarcasm.csv")
+    dataset = dataset.drop(["label", "score", "ups", "downs", "date", "created_utc"], 1)
+    dataset = dataset[["comment", "parent_comment", "author", "subreddit"]]
+    return dataset
+
+
+dataset = load_dataframe()
+corpus = dataset['comment'].to_list()
+
+# Add vector embeddings as column in df
+vectors = []
+for vector in sarcasm_embeddings:
+    vectors.append(list(vector.cpu().numpy()))
+
+dataset['vector'] = vectors
 
 def get_embeddings(comments):
     selected = df['Comment'].isin(comments)
@@ -14,19 +36,181 @@ def get_embeddings(comments):
     
     return embeddings
 
+def get_similar_comments(query, n):
+    """
+    Parameters
+    query (string): the text of the post
+    n (int): number of posts to recommend
+    
+    Returns df of top n similar comments
+    """
+    # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+    top_k = min(n, len(corpus))
+    query_embedding = embedder.encode(query, convert_to_tensor=True)
+    similarities = []
+    pairs = []
+
+    # We use cosine-similarity and torch.topk to find the highest 5 scores
+    cos_scores = util.pytorch_cos_sim(query_embedding.cpu(), sarcasm_embeddings)[0]
+    top_results = torch.topk(cos_scores, k=top_k)
+
+    for score, idx in zip(top_results[0], top_results[1]):
+        pairs.append(tuple((corpus[idx], score)))
+    
+    recommend_frame = []
+    for val in pairs:
+        recommend_frame.append({'Comment':val[0],'Similarity':val[1].numpy()})
+     
+    df = pd.DataFrame(recommend_frame)
+    df_sim = df.copy()
+    df_sim = df_sim = df_sim.set_index(['Comment'])
+    df = df.join(dataset.set_index('comment'), on='Comment')
+    return df, df_sim
+
+def calculate_quality(c, R, df, df_sim):
+    """
+    *add
+    """
+    quality = 0
+    rel_diversity = 0
+    
+    if len(R) == 0:
+        rel_diversity = 1
+        
+    vector = np.array(df['vector'][df['Comment'] == c].to_numpy()[0]).reshape(1, -1)
+    diversity = []
+    for item in R:
+        diversity.append(1 - cosine_similarity(vector, np.array(df_sim['vector'][df_sim['Comment'] == item].to_numpy()[0]).reshape(1, -1)))
+        
+    rel_diversity = sum(diversity)/len(R) # relative diversity
+    
+    similarity = df['Similarity'][df['Comment'] == c].to_numpy()[0] # similarity
+    
+    quality = rel_diversity[0][0] * similarity # quality
+    return quality
+
 
 def greedy_selection(query, num_to_recommend):
-    pass
+    """
+    Parameters
+    query (string): the text of the post
+    n (int): number of posts to recommend
+    
+    Returns df with diverse comments
+    """
+    C_prime = get_similar_comments(query, 500)[0]
+    
+    df_temp = C_prime.copy()
+    recommendations = ['dummy']
+    recommendations[0] = C_prime["Comment"][0]  # first item is always the one with the highest similarity
 
-def topic_diversification(query, num_to_recommend):
-    pass
+    index = df_temp[(df_temp.Comment == recommendations[0])].index
 
-def get_similar_comments(query, num_to_recommend):
-    pass
+    df_temp = df_temp.drop(index)
+    
+    # set num_to_recommend = 50 to get top 50 recommendations
+    for i in range(num_to_recommend):
+        qualities = {}
+        # Calculate the quality of each subreddit
+        for item in df_temp['Comment']:
+            qualities[item] = calculate_quality(item, recommendations, df_temp, C_prime)
 
-def compute_diversity():
-    pass
+        highest_quality = max(qualities.values())
+        highest_quality_subreddit = max(qualities, key= lambda x: qualities[x])
+        recommendations.append(highest_quality_subreddit)
 
-def compare_diversity():
-    pass
+        index = df_temp[(df_temp.Comment == recommendations[-1])].index
+        df_temp = df_temp.drop(index)
+        
+    similarities = []
+    for item in recommendations:
+        sim = C_prime['Similarity'][C_prime['Comment'] == item].to_numpy()[0]
+        similarities.append(sim)
+
+    pairs = list(zip(recommendations, similarities))
+    recommend_frame = []
+    for val in pairs:
+        recommend_frame.append({'Comment':val[0],'Similarity':val[1].item()})    
+
+    df_sim = pd.DataFrame(recommend_frame)
+    df = df_sim.copy()
+    df = df.join(dataset.set_index('comment'), on='Comment')
+    df_sim = df_sim.set_index(['Comment'])
+    df.reset_index()
+    df_sim.reset_index()
+    pd.set_option("display.max_colwidth", None)
+    return df, df_sim
+
+def topic_diversification(query, n):
+    """
+    Parameters
+    query (string): the text of the post
+    n (int): number of posts to recommend
+    
+    Returns df with diverse comments
+    """
+    N = 5 * n
+    C_prime = get_similar_comments(query, N)[0]
+    
+    # Prepare df for pariwise distance
+    df_ils = C_prime.copy()
+    df_ils = df_ils.set_index(['Comment'])
+    
+    ils = {}
+    # set ILS for first item
+    ils[df_ils.head(1)['Similarity'].index.values.item(0)] = df_ils.head(1)['Similarity'].values[0].item()
+    for i in range(2, N+1):
+        top_n = df_ils.head(i - 1)
+        top_n = top_n[['Similarity']]
+        bottom = df_ils.tail(len(df_ils) - i + 1)
+        bottom = bottom[['Similarity']]
+        for item in bottom.index:
+            rowData = bottom.loc[[item] , :]
+            top_n = top_n.append(rowData)
+            ils[item] = sum( [x for x in pdist(top_n)] ) / len(top_n) # ILS Calculation
+            top_n= top_n.drop(index=item)
             
+    
+    dissimilarity_rank = {k: v for k, v in sorted(ils.items(), key=lambda item: item[1], reverse=True)}
+    
+    # a,b ∈ [0,1]
+    a = 0.01
+    b = 0.99
+    new_rank = {}
+    dissimilarity_rank = OrderedDict(dissimilarity_rank)
+    for item in df_ils.index:
+        P = C_prime.index[C_prime['Comment'] == item]
+        Pd = list(dissimilarity_rank.keys()).index(item)
+        new_rank[item] = (a * P) + (b * Pd)
+        
+    final_ranks = {k: v for k, v in sorted(new_rank.items(), key=lambda item: item[1], reverse=False)}
+    
+    data = []
+    for comment, score in final_ranks.items():
+        data.append({'Comment': comment,'Rank': score[0]})
+
+    df_sim = pd.DataFrame(data)
+    ils_rank = []
+    for item in df_sim['Comment']:
+        ils_rank.append(dissimilarity_rank[item])
+
+    df_sim['ILS Score'] = ils_rank
+    df_sim = df_sim.sort_values(by=['Rank'], ascending=False)
+    df_sim = df_sim.head(n)
+    df = df_sim.copy()
+    df = df.join(dataset.set_index('comment'), on='Comment')
+    df_sim = df_sim.drop(columns=['Rank'])
+    df_sim = df_sim.set_index(['Comment'])
+    df.reset_index()
+    df_sim.reset_index()
+    pd.set_option("display.max_colwidth", None)
+    return df, df_sim
+
+def compute_diversity(df, n):
+    dis_similarity = [x for x in pdist(df)]
+    avg_dissim_greedy = (sum(dis_similarity))/((n/2)*(n-1))
+    return avg_dissim_greedy
+
+def compare_diversity(avg_dissim_algo, avg_dissim_control):
+    percent_change = ((avg_dissim_algo - avg_dissim_control)/avg_dissim_control)*100
+    return round(percent_change, 2)
