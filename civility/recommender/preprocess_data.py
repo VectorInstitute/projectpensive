79a2b416ad36d44e2c534a49c747191ab2a3d9a9
MichaelNasello <mlnasell@uwaterloo.ca>
2021-08-13 10:47:25 -0400

    Script for preprocessing data

--- civility/recommender/preprocess_data.py
+++ civility/recommender/preprocess_data.py
@@ -10,22 +10,24 @@ if __name__ == "__main__":
     data = data.drop(["label", "score", "ups", "downs", "date", "created_utc"], 1)
     data = data[["comment", "parent_comment", "author", "subreddit"]]
 
-    # Remove data points where the comment is 3 words or less
-    ids_to_drop = []
-    for comment in data.comment.items():
-        len_comment = len(str(comment[1]).split())
-        if len_comment <= 3:
-            ids_to_drop.append(comment[0])
+    # Remove data points where the comment is 9 words or less
+    data["comment"] = data["comment"].astype(str)
+    strings = data.comment.values
+    string_cond = [len(s.split()) <= 9 for s in strings]
+    ids_to_drop = [index for index, cond in enumerate(string_cond) if cond]
     data = data.drop(ids_to_drop)
     data = data.reset_index(drop=True)
 
-    # Remove data points where the author makes less than 2 comments in the dataset
-    ids_to_drop = []
-    author_value_counts = data.author.value_counts() > 2
+    # Remove data points where the author makes less than 4 comments in the dataset
+    good_authors = []
+    author_value_counts = data.author.value_counts() > 7
     for author, condition in author_value_counts.items():
-        if not condition:
-            ids_to_drop += data.index[data["author"] == author].tolist()
-    data = data.drop(ids_to_drop)
-    data = data.reset_index(drop=True)
+        if condition:
+            good_authors.append(author)
+        else:
+            # Since author_value_counts is given in descending order, once one author does not meed the condition
+            # all following authors will not meed the condition
+            break
+    data = data.loc[data["author"].isin(good_authors)]
 
     data.to_csv("train-balanced-sarcasm-processed.csv")
