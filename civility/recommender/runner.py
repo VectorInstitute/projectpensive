ecdbd7061521bb66c5f845c6b64b85f21dd5821f
MichaelNasello <mlnasell@uwaterloo.ca>
2021-08-13 10:48:05 -0400

    Runner for recommender engine

---
+++ civility/recommender/runner.py
@@ -0,0 +1,40 @@
+import torch
+
+from model import RecommenderEngine
+
+
+class RecommenderEngineRunner:
+    """
+    Runs data through model to get recommendations.
+    """
+
+    def __init__(self, path_to_model, data, n_factors, device):
+        # Load model and weights
+        self.model = RecommenderEngine(data, n_factors, device)
+        self.model.load_state_dict(torch.load(path_to_model))
+
+    def run_model(self, query, data_frame):
+
+        # Add match_score column to data_frame
+        data_frame = data_frame.assign(match_score=0.0)
+
+        for comment in data_frame.comment.items():
+            # Compute match score
+            match_score = self.model.forward(
+                query["author"],
+                query["subreddit"],
+                comment[1]
+            )
+            data_frame.at[comment[0], "match_score"] = round(match_score, 3)
+
+        # Reorder data_frame based on match_score
+        data_frame = data_frame.sort_values("match_score", ascending=False)
+
+        # Remove match_score
+        data_frame = data_frame[["comment", "parent_comment", "author", "subreddit"]]
+
+        return data_frame
+
+
+if __name__ == "__main__":
+    print(0)
