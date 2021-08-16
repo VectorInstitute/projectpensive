e5ba8cbe4cf02d8889d7f18b88a7e909aa747548
michaelnasello <mlnasell@uwaterloo.ca>
2021-08-16 16:50:40 -0400

    Removing download_model_checkpoints.sh

--- civility/classifier/download_model_checkpoints.sh
+++
@@ -1,5 +0,0 @@
-echo Downloading model checkpoints from remote server...
-
-mkdir results
-scp mnas@v.vectorinstitute.ai:baiso/baiso/civility/classifier/output.log results/output.log
-scp -r mnas@v.vectorinstitute.ai:baiso/baiso/civility/classifier/results/final_model results/final_model
