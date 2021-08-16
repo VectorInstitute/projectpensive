6547addfef11d8b97fec6267b8d52f54809e4b1b
michaelnasello <mlnasell@uwaterloo.ca>
2021-08-16 14:03:59 -0400

    Updates to evaluation.py

--- demo/evaluation.py
+++ demo/evaluation.py
@@ -10,9 +10,46 @@ def evaluation():
     # Civility
     st.subheader("Civility")
     with st.expander("Read more"):
-        st.write(
-            "To evaluate the performance of the model, we will compare it with the Jigsaw Perspective API..."
+        # interesting examples
+        # eval against jigsaw
+        st.write("**Performance Comparison with Jigsaw Perspective**")
+        st.markdown(
+            """To evaluate the model, we will compare its performance with the [Jigsaw Perspective API]
+            (https://www.perspectiveapi.com/). The Jigsaw Perspective tool is built by [Conversation AI]
+            (https://conversationai.github.io/), the maintainers of our `civil_comments` dataset."""
         )
+        st.write(
+            "The following test was run on a subset of the test dataset."
+        )
+        st.code(
+            "jigsaw_precision, jigsaw_recall, jigsaw_f1_score, _ = sklearn.metrics.precision_recall_fscore_support("
+            "\n\ty_true=dataset_is_toxic,"
+            "\n\ty_pred=jigsaw_is_toxic,"
+            '\n\taverage="weighted"'
+            "\n)"
+            "\nmodel_precision, model_recall, model_f1_score, _ = sklearn.metrics.precision_recall_fscore_support("
+            "\n\ty_true=dataset_is_toxic,"
+            "\n\ty_pred=model_is_toxic,"
+            '\n\taverage="weighted"'
+            "\n)"
+        )
+        st.write(
+            "Output:"
+        )
+        st.code(
+            "Jigsaw:"
+            "\n\tPrecision: 0.951, Recall: 0.913, F1: 0.926"
+            "\nModel:"
+            "\n\tPrecision: 0.934, Recall: 0.938, F1: 0.936"
+        )
+        st.markdown(
+            "As can be seen, our transformer model is achieving similar performance. Unfortunately, Jigsaw's model is "
+            "not open source, and we cannot compare model sizes. However, [they state]"
+            "(https://developers.perspectiveapi.com/s/about-the-api-model-cards) that they are using a distilled "
+            "versions of `BERT`."
+        )
+
+        st.write("**Interesting Examples**")
 
     # Diversity
     st.subheader("Diversity")
@@ -22,6 +59,8 @@ def evaluation():
     # Recommender
     st.subheader("Recommender Engine")
     with st.expander("Read more"):
+        # interesting examples
+        # mention that not limited to subreddit in query
         st.write(
             ""
         )
