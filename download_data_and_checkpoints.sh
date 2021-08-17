1f571d6f29dabd081fbb542fc0eaadfaaac5c6dc
michaelnasello <mlnasell@uwaterloo.ca>
2021-08-17 10:42:34 -0400

    Updates to README.md and new download script

--- download_data_and_checkpoints.sh
+++ download_data_and_checkpoints.sh
@@ -1,13 +1,16 @@
+USER_ID=$1
+
 echo Downloading data...
-scp mnas@v.vectorinstitute.ai:/checkpoint/gshensvm/BAISO_checkpoints/data/train-balanced-sarcasm-processed.csv civility/recommender/
+scp $USER_ID@v.vectorinstitute.ai:/checkpoint/gshensvm/BAISO_checkpoints/data/train-balanced-sarcasm-processed.csv civility/recommender/
 
 echo Downloading civility filter checkpoint...
 mkdir civility/classifier/results/
-scp -r mnas@v.vectorinstitute.ai:/checkpoint/gshensvm/BAISO_checkpoints/model_checkpoints/civility_model civility/classifier/results/final_model
+scp -r $USER_ID@v.vectorinstitute.ai:/checkpoint/gshensvm/BAISO_checkpoints/model_checkpoints/civility_model civility/classifier/results/final_model
 
 echo Downloading diversity embeddings...
-scp -r mnas@v.vectorinstitute.ai:/checkpoint/gshensvm/BAISO_checkpoints/embeddings diversity/datasets/
+mkdir diversity/datasets
+scp $USER_ID@v.vectorinstitute.ai:/checkpoint/gshensvm/BAISO_checkpoints/embeddings/subreddit_embeddings.csv diversity/datasets/
+scp $USER_ID@v.vectorinstitute.ai:/checkpoint/gshensvm/BAISO_checkpoints/embeddings/sarcasm-embeddings-processed.pt diversity/datasets/
 
 echo Downloading recommender engine checkpoint...
-mkdir demo/data
-scp -r mnas@v.vectorinstitute.ai:/checkpoint/gshensvm/BAISO_checkpoints/model_checkpoints/recommender_model civility/recommender/final_model
+scp -r $USER_ID@v.vectorinstitute.ai:/checkpoint/gshensvm/BAISO_checkpoints/model_checkpoints/recommender_model civility/recommender/final_model
